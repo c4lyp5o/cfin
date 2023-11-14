471324e1a2ed54c01e7cc828e456814d6853b915
c4lyp5o <calypso@calypsocloud.one>
2023-11-14 17:03:59 +0800

    fix: routes
    call function dari controller untuk run route

--- routes/fileRoutes.js
+++ routes/fileRoutes.js
@@ -1,74 +1,26 @@
-// routes/fileRoutes.js
-import fs from 'fs';
-import path from 'path';
-import prisma from '../database/client.js';
+import {
+  getAllFiles,
+  streamFile,
+  createFile,
+  updateFile,
+  deleteFile,
+} from '../controllers/fileController.js';
 
 export default function (fastify, opts, done) {
   // Get all files
-  fastify.get('/files', async (request, reply) => {
-    const files = await prisma.sharedFiles.findMany();
-    reply.send(files);
-  });
+  fastify.get('/files', getAllFiles);
 
-  // Get all files
-  fastify.get('/files/stream', async (request, reply) => {
-    try {
-      // get req query named id
-      const id = request.query.id;
-
-      // get file from db
-      const file = await prisma.sharedFiles.findUnique({
-        where: { id: Number(id) },
-      });
-
-      // get file path from db
-      const filePath = file.filePath;
-
-      // get file size from db
-      const stat = fs.statSync(filePath);
-
-      // get file extension from db
-      const fileExtension = file.fileExtension;
-
-      // set response headers
-      reply.headers({
-        'Content-Length': stat.size,
-        'Content-Type': `video/${fileExtension}`,
-      });
-
-      // stream file
-      const stream = fs.createReadStream(filePath);
-      reply.send(stream);
-    } catch (err) {
-      console.log(err);
-      reply.code(500).send({ error: 'Failed to read file' });
-    }
-  });
+  // Stream video files
+  fastify.get('/files/stream', streamFile);
 
   // Create a file
-  fastify.post('/files', async (request, reply) => {
-    const file = await prisma.sharedFiles.create({
-      data: request.body,
-    });
-    reply.send(file);
-  });
+  fastify.post('/files', createFile);
 
   // Update a file
-  fastify.put('/files/:id', async (request, reply) => {
-    const file = await prisma.sharedFiles.update({
-      where: { id: Number(request.params.id) },
-      data: request.body,
-    });
-    reply.send(file);
-  });
+  fastify.put('/files/:id', updateFile);
 
   // Delete a file
-  fastify.delete('/files/:id', async (request, reply) => {
-    const file = await prisma.sharedFiles.delete({
-      where: { id: Number(request.params.id) },
-    });
-    reply.send(file);
-  });
+  fastify.delete('/files/:id', deleteFile);
 
   done();
 }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,112 +1,30 @@
-// routes/userRoutes.js
-import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
-
-import prisma from '../database/client.js';
+import {
+  getAllUsers,
+  createUser,
+  loginUser,
+  validateUser,
+  updateUser,
+  deleteUser,
+} from '../controllers/userController.js';
 
 export default function (fastify, opts, done) {
   // Get all users
-  fastify.get('/users', async (request, reply) => {
-    const users = await prisma.userConfig.findMany();
-    reply.send(users);
-  });
+  fastify.get('/users', getAllUsers);
 
   // Create a user
-  fastify.post('/users/create', async (request, reply) => {
-    const { username, password } = request.body;
-
-    // Check if user already exists
-    const existingUser = await prisma.userConfig.findUnique({
-      where: { username },
-    });
-
-    if (existingUser) {
-      reply.status(400).send({ error: 'Username already exists' });
-      return;
-    }
-
-    // check if there are any users in the database
-    const users = await prisma.userConfig.findMany();
-
-    // If there are no users, make this user an admin
-    const role = users.length === 0 ? 'admin' : 'user';
-
-    // Hash password
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password, salt);
-
-    const user = await prisma.userConfig.create({
-      data: {
-        username,
-        password: hashedPassword,
-        role,
-      },
-    });
-
-    reply.send(user);
-  });
+  fastify.post('/users/create', createUser);
 
   // login user
-  fastify.post('/users/login', async (request, reply) => {
-    const { username, password } = request.body;
-
-    // Check if user exists
-    const existingUser = await prisma.userConfig.findUnique({
-      where: { username },
-    });
-
-    if (!existingUser) {
-      reply.status(400).send({ error: 'Username does not exist' });
-      return;
-    }
-
-    // Check if password is correct
-    const validPassword = await bcrypt.compare(password, existingUser.password);
-
-    if (!validPassword) {
-      reply.status(400).send({ error: 'Invalid password' });
-      return;
-    }
-
-    // Create and sign a JWT token
-    const token = jwt.sign({ id: existingUser.id }, process.env.JWT_SECRET);
-
-    reply.send({ token, username: existingUser.username });
-  });
+  fastify.post('/users/login', loginUser);
 
   // validate token
-  fastify.get('/users/validate', async (request, reply) => {
-    const token = request.headers.authorization?.split(' ')[1];
-
-    if (!token) {
-      reply.status(400).send({ error: 'Token is required' });
-      return;
-    }
-
-    try {
-      jwt.verify(token, process.env.JWT_SECRET);
-      reply.send({ valid: true });
-    } catch (err) {
-      reply.status(401).send({ valid: false });
-    }
-  });
+  fastify.get('/users/validate', validateUser);
 
   // Update a user
-  fastify.put('/users/:id', async (request, reply) => {
-    const user = await prisma.userConfig.update({
-      where: { id: Number(request.params.id) },
-      data: request.body,
-    });
-    reply.send(user);
-  });
+  fastify.put('/users/:id', updateUser);
 
   // Delete a user
-  fastify.delete('/users/:id', async (request, reply) => {
-    const user = await prisma.userConfig.delete({
-      where: { id: Number(request.params.id) },
-    });
-    reply.send(user);
-  });
+  fastify.delete('/users/:id', deleteUser);
 
   done();
 }
