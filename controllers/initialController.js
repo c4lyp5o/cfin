54b85e0bb5361d2b95eb3e67704cb6ff099e97f6
c4lyp5o <calypso@calypsocloud.one>
2024-02-01 23:27:22 +0800

    fix: wrap everything in try catch

--- controllers/initialController.js
+++ controllers/initialController.js
@@ -1,40 +1,47 @@
 import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
 import { v4 as uuidv4 } from 'uuid';
 
 import prisma from '../database/client.js';
 
 const initialCheck = async (req, reply) => {
-  const users = await prisma.userConfig.findMany();
-  if (users.length === 0) {
-    reply.send({ showRegistration: true });
-  } else {
-    reply.send({ showRegistration: false });
+  try {
+    const users = await prisma.userConfig.findMany();
+    if (users.length === 0) {
+      reply.send({ showRegistration: true });
+    } else {
+      reply.send({ showRegistration: false });
+    }
+  } catch (err) {
+    reply.status(500).send({ message: err.message });
   }
 };
 
 const initialRegistration = async (request, reply) => {
-  const { username, password } = request.body;
+  try {
+    const { username, password } = request.body;
 
-  // Hash password
-  const salt = await bcrypt.genSalt(10);
-  const hashedPassword = await bcrypt.hash(password, salt);
+    // Hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
 
-  const user = await prisma.userConfig.create({
-    data: {
-      username,
-      password: hashedPassword,
-      role: 'admin',
-    },
-  });
+    const user = await prisma.userConfig.create({
+      data: {
+        username,
+        password: hashedPassword,
+        role: 'admin',
+      },
+    });
 
-  reply.send(user);
+    reply.send(user);
+  } catch (err) {
+    reply.status(500).send({ message: err.message });
+  }
 };
 
 const loginUser = async (request, reply) => {
-  const { username, password } = request.body;
-
   try {
+    const { username, password } = request.body;
+
     // Check if user exists
     const existingUser = await prisma.userConfig.findUnique({
       where: { username },
