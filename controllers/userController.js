810df4226f652385a8d71d7da4bffcc2b0135de4
c4lyp5o <calypso@calypsocloud.one>
2024-02-01 23:29:31 +0800

    fix: wrap everything in try catch, remove registering as admin since initial has that covered

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,42 +3,46 @@ import bcrypt from 'bcrypt';
 import prisma from '../database/client.js';
 
 const getAllUsers = async (request, reply) => {
-  const users = await prisma.userConfig.findMany();
-  reply.send(users);
+  try {
+    const users = await prisma.userConfig.findMany();
+    reply.send(users);
+  } catch (err) {
+    console.log(err);
+    reply.code(500).send({ error: err.message });
+  }
 };
 
 const createUser = async (request, reply) => {
-  const { username, password, role } = request.body;
+  try {
+    const { username, password, role } = request.body;
 
-  // Check if user already exists
-  const existingUser = await prisma.userConfig.findUnique({
-    where: { username },
-  });
+    // Check if user already exists
+    const existingUser = await prisma.userConfig.findUnique({
+      where: { username },
+    });
 
-  if (existingUser) {
-    reply.status(400).send({ error: 'Username already exists' });
-    return;
+    if (existingUser) {
+      reply.status(400).send({ error: 'Username already exists' });
+      return;
+    }
+
+    // Hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = await prisma.userConfig.create({
+      data: {
+        username,
+        password: hashedPassword,
+        role: role,
+      },
+    });
+
+    reply.send(user);
+  } catch (err) {
+    console.log(err);
+    reply.code(500).send({ error: err.message });
   }
-
-  // check if there are any users in the database
-  const users = await prisma.userConfig.findMany();
-
-  // If there are no users, make this user an admin
-  const designatedRole = users.length === 0 ? 'admin' : role;
-
-  // Hash password
-  const salt = await bcrypt.genSalt(10);
-  const hashedPassword = await bcrypt.hash(password, salt);
-
-  const user = await prisma.userConfig.create({
-    data: {
-      username,
-      password: hashedPassword,
-      role: designatedRole,
-    },
-  });
-
-  reply.send(user);
 };
 
 const updateUser = async (request, reply) => {
