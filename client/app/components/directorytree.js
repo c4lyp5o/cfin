905e557c5d7a2d2c842040f1c2290bc907825360
c4lyp5o <calypso@calypsocloud.one>
2024-02-11 02:27:48 +0800

    fix: *nix based os covered

--- client/app/components/directorytree.js
+++ client/app/components/directorytree.js
@@ -1,72 +1,107 @@
 import React, { useState, useEffect } from 'react';
-import { FolderIcon } from '@heroicons/react/24/outline';
+import { FolderIcon, DocumentIcon } from '@heroicons/react/24/outline';
 
-const DirectoryTree = ({ setFolder }) => {
+const DirectoryTree = ({ folder, setFolder }) => {
+  const [allDrives, setAllDrives] = useState([]);
   const [drive, setDrive] = useState(null);
   const [path, setPath] = useState('/');
   const [contents, setContents] = useState([]);
+  const [error, setError] = useState(null);
+
+  const joinPath = (...args) => args.join('/').replace(/\/+/g, '/');
 
   const handleItemClick = (item) => {
     if (drive) {
-      setPath(`${path}/${item.name}`);
-      setFolder(`${drive}${path}/${item.name}`);
+      const newPath = joinPath(path, item.name);
+      setPath(newPath);
+      setFolder(joinPath(drive, newPath));
     } else {
       setDrive(item.name);
       setPath('/');
+      setFolder(item.name);
     }
+  };
+
+  const handleGoBackDirectory = () => {
+    const driveNames = allDrives.map((drive) => drive.name);
+    let newDrive = drive;
+    let newPath = path;
+    let newFolder = folder;
+
+    if (driveNames.includes(folder)) {
+      newDrive = null;
+      newPath = null;
+      newFolder = null;
+    } else {
+      newPath = path.split('/').slice(0, -1).join('/');
+      if (drive === '/' && newPath !== '') {
+        newFolder = newPath;
+      } else {
+        newFolder = `${drive}${newPath}`;
+      }
+    }
+
+    setDrive(newDrive);
+    setPath(newPath);
+    setFolder(newFolder);
   };
 
   useEffect(() => {
     const readDir = async () => {
       try {
         let response;
+
         if (drive) {
-          response = await fetch(
-            `/api/v1/folders/read?path=${encodeURIComponent(drive + path)}`
-          );
+          const test = encodeURIComponent(`${drive}${path}`);
+          response = await fetch(`/api/v1/folders/read?path=${test}`);
         } else {
           response = await fetch(`/api/v1/folders/drives`);
         }
 
         if (!response.ok) {
-          throw new Error('Error reading directory');
+          throw new Error('Error reading drives or folders.');
         }
 
         const data = await response.json();
+        if (!drive) setAllDrives(data);
         setContents(data);
       } catch (error) {
-        console.error('An error occurred:', error);
+        setError(error.message);
       }
     };
     readDir();
-  }, [drive, path]);
+  }, [drive, path, setAllDrives, setContents]);
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
 
   return (
     <div className='flex flex-wrap'>
       <button
+        type='button'
         className='mb-3 w-full px-4 py-2 text-white bg-green-500 rounded-full hover:bg-green-600'
         aria-label='Current Path'
       >
-        Current Path: {path}
+        Current Path: {folder}
       </button>
       {/* back button */}
-      {path !== '/' && (
+      {drive && (
         <div
           className='cursor-pointer text-black hover:text-blue-500 mr-4'
-          onClick={() => handleItemClick(item)}
+          onClick={handleGoBackDirectory}
           aria-label='Back'
         >
           ..
         </div>
       )}
-      {contents.map((item, index) => (
+      {contents.map((item) => (
         <div
-          key={index}
-          className='cursor-pointer text-black hover:text-blue-500 mr-4 flex items-center'
-          onClick={() => {
-            setPath(`${path}/${item.name}`);
-            setFolder(`${path}/${item.name}`);
-          }}
+          key={item.name}
+          className={`cursor-pointer text-black hover:text-blue-500 mr-4 flex items-center ${
+            item.type === 'file' ? 'pointer-events-none' : ''
+          }`}
+          onClick={() => item.type !== 'file' && handleItemClick(item)}
           aria-label='Folder'
         >
           <FolderIcon className='h-5 w-5 mr-2' />
